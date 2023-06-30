2eb58cf0ca174d4a21d39255e782f5f5d8463e46
GISLAINE DE SOUZA NASCIMENTO <gislaine.1976@aluno.pr.senac.br>
2023-06-30 16:55:12 -0300

    feito construção tijolos

--- curriculo/Jogo Quebra Tijolo/script.js
+++ curriculo/Jogo Quebra Tijolo/script.js
@@ -1,59 +1,162 @@
 var canvas = document.getElementById("gameCanvas");
 var desenho = canvas.getContext("2d");
 
-//configurar raquete
+//configurar rebatedor
 var raqueteAltura = 7;
 var raqueteLargura = 75;
 var raqueteX = (canvas.width - raqueteLargura) / 2;
 var velocidadeRaquete = 7;
+ 
 
+//configurar a bola
 var bolaRadius = 10;
 var bolaX = canvas.width / 2;
 var bolaY = canvas.height - 30;
 var bolaDX = 2;
 var bolaDY = -2;
 
+var tijolosPorLinha = 5;
+var tijolosPorColuna = 9;
+var tijoloLargura = 50;
+var tijoloAltura = 20;
+var tijoloespacamento = 10;
+var espacamentoSuperiorQuadro = 30;
+var espacamentoEsquerdoQuadro = 30;
+var tijolos = [];
+
+for(var coluna=0; coluna< tijolosPorColuna; coluna++ ){
+    tijolos[coluna] = []
+
+    for(var linha=0; linha < tijolosPorLinha; linha ++){
+        tijolos[coluna][linha] = {x:0, y:0, ativo:1 }
+    }
+}  
+
+ 
 var setaDireita = false;
 var setaEsquerda = false;
-
-
-document.addEventListener("keydown", descerDaTecla);
+ 
+document.addEventListener("keydown", descerDaTecla);                                                                          
 document.addEventListener("keyup", subirDaTecla);
 
-function descerDaTecla(tecla) {
-
-    if(tecla.key === "Right" || tecla.key === "ArrowRight"){
+function descerDaTecla(tecla){
+    if(tecla.key === "Right" || tecla.key === "ArrowRight" ){
         setaDireita = true;
-    } else if (tecla.key === "Left" || tecla.key === "ArrowLeft"){
-
-   
+     }else if(tecla.key === "Left" || tecla.key === "ArrowLeft"){
         setaEsquerda = true;
+        }
 
-}
-}
-function subirDaTecla() {
+    }
+    function subirDaTecla(tecla){
+        if(tecla.key === "Right" || tecla.key === "ArrowRight" ){
+            setaDireita = false;
+         }else if(tecla.key === "Left" || tecla.key === "ArrowLeft"){
+            setaEsquerda = false;
+         }
+    }
 
-}
 
-
-function desenharRaquete() {
+function desenharRaquete(){
     desenho.beginPath();
     desenho.rect(raqueteX, canvas.height - raqueteAltura, raqueteLargura, raqueteAltura);
-    desenho.fillStyle = " dark";
+    desenho.fillStyle = "black";
     desenho.fill();
     desenho.closePath();
 }
+function desenharBola(){
+    desenho.beginPath();
+    desenho.arc(bolaX, bolaY, bolaRadius, 0, Math.PI * 2);
+    desenho.fillStyle = "black";
+    desenho.fill();
+    desenho.closePath();
 
-function desenhar() {
+}
 
-    desenho.clearRect(0, 0, canvas.width, canvas.height);
+function desenhartijolos(){
+    for(var coluna=0; coluna < tijolosPorColuna; coluna++){
+        for(var linha=0; linha < tijolosPorLinha; linha++){
+            if(tijolos[coluna][linha].ativo == 1){
+              var tijoloX = (coluna * (tijoloLargura + tijoloespacamento)+ espacamentoEsquerdoQuadro);
+              var tijoloY = (linha * (tijoloAltura + tijoloespacamento) + espacamentoSuperiorQuadro);
+              
+              tijolos[coluna][linha].x = tijoloX;
+              tijolos[coluna][linha].y = tijoloY;  
+
+              desenho.beginPath();
+              desenho.rect(tijoloX, tijoloY, tijoloLargura, tijoloAltura);
+              desenho.fillStyle = " rgba(41, 63, 82, 0.507)";
+              desenho.fill();
+              desenho.closePath();
+
+
+
+            }
+        }
+    }
+}
+
+function detectarColisao(){
+    for(var coluna = 0; coluna < tijolosPorColuna; coluna++) {
+        for(var linha = 0; linha < tijolosPorLinha; linha++) {
+
+        var tijolo = tijolos[coluna][linha];
+
+        if(tijolo.ativo === 1){
+
+            if(bolaX > tijolo.x
+                && bolaX < tijolo.x + tijoloLargura
+                && bolaY > tijolo.y
+                && bolaY < tijolo.y + tijoloAltura){
+                    bolaDY = -bolaDY;
+                    tijolo.ativo = 0; 
+
+                }
+        }
+    }
+}
+}
+
+function desenhar(){
+    desenho.clearRect(0 ,0, canvas.width, canvas.height);
     desenharRaquete();
-    if(setaDireita === true && raqueteX < canvas.width - raqueteLargura) {
-        raqueteX = raqueteX + velocidadeRaquete;
-    }else if(setaEsquerda && raqueteX > 0){
-        raqueteX = raqueteX - velocidadeRaquete;
+    desenharBola();
+    desenhartijolos();
+    detectarColisao();
+
+    if (bolaX + bolaDX > canvas.width - bolaRadius || bolaX + bolaDX < bolaRadius){
+        
+        bolaDX = -bolaDX;
+
     }
 
-requestAnimationFrame(desenhar)
-}
-desenhar();
+    if(bolaY + bolaDY < bolaRadius){
+
+        bolaDY = -bolaDY;
+
+    } else if(bolaY + bolaDY > canvas.height - bolaRadius){
+
+        if(bolaX > raqueteX && bolaX < raqueteX + raqueteLargura){
+
+            bolaDY = -bolaDY;
+
+            }else{
+
+                document.location.reload();
+            }
+        }
+
+        if(setaDireita === true && raqueteX < canvas.width - raqueteLargura){
+            raqueteX = raqueteX + velocidadeRaquete;
+    
+        }
+        else if(setaEsquerda === true && raqueteX > 0){
+            raqueteX = raqueteX - velocidadeRaquete;
+        }
+        bolaX = bolaX + bolaDX;
+        bolaY = bolaY + bolaDY;
+
+        requestAnimationFrame(desenhar)
+
+    }
+
+ desenhar();
