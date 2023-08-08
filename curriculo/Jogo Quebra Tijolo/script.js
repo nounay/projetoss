1f05dfe4c0d6776df523e9fb303d4aaa419e0fea
GISLAINE DE SOUZA NASCIMENTO <gislaine.1976@aluno.pr.senac.br>
2023-08-08 14:58:36 -0300

    criacao do jogo breakout

--- curriculo/Jogo Quebra Tijolo/script.js
+++ curriculo/Jogo Quebra Tijolo/script.js
@@ -1,3 +1,4 @@
+//definir área do canvas
 var canvas = document.getElementById("gameCanvas");
 var desenho = canvas.getContext("2d");
 
@@ -12,25 +13,100 @@ var velocidadeRaquete = 7;
 var bolaRadius = 10;
 var bolaX = canvas.width / 2;
 var bolaY = canvas.height - 30;
-var bolaDX = 2;
-var bolaDY = -2;
+var bolaDX = 4;
+var bolaDY = -4;
 
-var tijolosPorLinha = 5;
-var tijolosPorColuna = 9;
-var tijoloLargura = 50;
-var tijoloAltura = 20;
-var tijoloespacamento = 10;
+var tijolosPorLinha = 4;
+var tijolosPorColuna = 6;
+var tijolosLargura = 75;
+var tijolosAltura = 20;
+var tijolosEspacamento = 8;
 var espacamentoSuperiorQuadro = 30;
 var espacamentoEsquerdoQuadro = 30;
 var tijolos = [];
 
-for(var coluna=0; coluna< tijolosPorColuna; coluna++ ){
-    tijolos[coluna] = []
+var totalPontuacao = tijolosPorLinha * tijolosPorColuna * 10;
+var pontuacao = 0;
 
-    for(var linha=0; linha < tijolosPorLinha; linha ++){
-        tijolos[coluna][linha] = {x:0, y:0, ativo:1 }
+function facil(){
+    raqueteLargura = 90;
+    tijolosPorLinha = 3;
+    tijolosPorColuna = 6;
+    tijoloLargura = 90;
+    tijoloAltura = 40;
+    bolaRadius = 10;
+    bolaDX = 2;
+    bolaDY = 2;
+    totalPontuacao = tijolosPorLinha * tijolosPorColuna * 10;
+    pontuacao = 0;
+    bolaX = canvas.width / 2;
+    bolaY = canvas.height - 30;
+    iniciarTijolos();
+
+}
+
+function medio(){
+    raqueteLargura = 70;
+    tijolosPorLinha = 4;
+    tijolosPorColuna = 6;
+    tijoloLargura = 80;
+    tijoloAltura = 40;
+    bolaRadius = 10;
+    bolaDX = 4;
+    bolaDY = 4;
+    totalPontuacao = tijolosPorLinha * tijolosPorColuna * 10;
+    pontuacao = 0;
+    bolaX = canvas.width / 2;
+    bolaY = canvas.height - 30;
+    iniciarTijolos();
+
+}
+
+function dificil(){
+    raqueteLargura = 50;
+    tijolosPorLinha = 5;
+    tijolosPorColuna = 6;
+    tijoloLargura = 80;
+    tijoloAltura = 40;
+    bolaRadius = 10;
+    bolaDX = 5;
+    bolaDY = 5;
+    totalPontuacao = tijolosPorLinha * tijolosPorColuna * 10;
+    pontuacao = 0;
+    bolaX = canvas.width / 2;
+    bolaY = canvas.height - 30;
+    iniciarTijolos();
+}
+ 
+function impossivel(){
+    raqueteLargura = 60;
+    tijolosPorLinha = 6;
+    tijolosPorColuna = 7;
+    tijoloLargura = 80;
+    tijoloAltura = 40;
+    bolaRadius = 10;
+    bolaDX = 7;
+    bolaDY = 7;
+    totalPontuacao = tijolosPorLinha * tijolosPorColuna * 10;
+    pontuacao = 0;
+    bolaX = canvas.width / 2;
+    bolaY = canvas.height - 30;
+    iniciarTijolos();
+}
+
+function iniciarTijolos(){
+    for(var coluna=0; coluna< tijolosPorColuna; coluna++ ){
+        tijolos[coluna] = []
+    
+        for(var linha=0; linha < tijolosPorLinha; linha ++){
+    
+            tijolos[coluna][linha] = {x:0, y:0, ativo:1 }
+        }
     }
-}  
+    
+}
+iniciarTijolos();
+
 
  
 var setaDireita = false;
@@ -59,36 +135,38 @@ function descerDaTecla(tecla){
 function desenharRaquete(){
     desenho.beginPath();
     desenho.rect(raqueteX, canvas.height - raqueteAltura, raqueteLargura, raqueteAltura);
-    desenho.fillStyle = "black";
+    desenho.fillStyle = "blue";
     desenho.fill();
     desenho.closePath();
+    
 }
 function desenharBola(){
     desenho.beginPath();
     desenho.arc(bolaX, bolaY, bolaRadius, 0, Math.PI * 2);
-    desenho.fillStyle = "black";
+    desenho.fillStyle = "gray";
     desenho.fill();
     desenho.closePath();
-
 }
-
-function desenhartijolos(){
+function desenharTijolos(){
     for(var coluna=0; coluna < tijolosPorColuna; coluna++){
-        for(var linha=0; linha < tijolosPorLinha; linha++){
+        for(var linha=0; linha < tijolosPorLinha; linha ++){
+
             if(tijolos[coluna][linha].ativo == 1){
-              var tijoloX = (coluna * (tijoloLargura + tijoloespacamento)+ espacamentoEsquerdoQuadro);
-              var tijoloY = (linha * (tijoloAltura + tijoloespacamento) + espacamentoSuperiorQuadro);
-              
-              tijolos[coluna][linha].x = tijoloX;
-              tijolos[coluna][linha].y = tijoloY;  
+                
+                var tijoloX = (coluna * (tijolosLargura + tijolosEspacamento)+ espacamentoEsquerdoQuadro);
+                var tijoloY = (linha * (tijolosAltura + tijolosEspacamento)+ espacamentoSuperiorQuadro);
 
-              desenho.beginPath();
-              desenho.rect(tijoloX, tijoloY, tijoloLargura, tijoloAltura);
-              desenho.fillStyle = " rgba(41, 63, 82, 0.507)";
-              desenho.fill();
-              desenho.closePath();
+                tijolos[coluna][linha].x = tijoloX;
+                tijolos[coluna][linha].y = tijoloY;
+
+                desenho.beginPath();
+                desenho.rect(tijoloX, tijoloY, tijolosLargura, tijolosAltura);
+                desenho.fillStyle = "blue";
+                desenho.fill();
+                desenho.closePath();
 
 
+                
 
             }
         }
@@ -96,44 +174,108 @@ function desenhartijolos(){
 }
 
 function detectarColisao(){
-    for(var coluna = 0; coluna < tijolosPorColuna; coluna++) {
-        for(var linha = 0; linha < tijolosPorLinha; linha++) {
+    for(var coluna=0; coluna < tijolosPorColuna; coluna++){
+        for(var linha=0; linha < tijolosPorLinha; linha++){
 
-        var tijolo = tijolos[coluna][linha];
+            var tijolo = tijolos[coluna][linha];
 
-        if(tijolo.ativo === 1){
+            if(tijolo.ativo === 1){
 
-            if(bolaX > tijolo.x
-                && bolaX < tijolo.x + tijoloLargura
-                && bolaY > tijolo.y
-                && bolaY < tijolo.y + tijoloAltura){
-                    bolaDY = -bolaDY;
-                    tijolo.ativo = 0; 
+                if(bolaX > tijolo.x
+                    && bolaX - bolaRadius < tijolo.x + tijolosLargura
+                    && bolaY + bolaRadius > tijolo.y
+                    && bolaY - bolaRadius < tijolo.y + tijolosAltura){
+                        bolaDY = -bolaDY;
+                        tijolo.ativo = 0;
+                        tela = document.getElementById("ponto");
+                        pontuacao = pontuacao + 10;
+                        tela.innerHTML = "Score: "+ pontuacao;
+                        gerarEfeitoSonoro('moeda.mp3');
+                        
 
+                        if(pontuacao === totalPontuacao){
+                            vitoria();
+                        }
+                    }
                 }
+            }
         }
     }
+
+
+    contador = 0;
+
+function gameover(){
+    var gameover = document.getElementById("gameover");
+    gameover.style.display = "block";
+
+    if(contador < 1){
+
+    gerarEfeitoSonoro('gameover.mp3')
+    
+    bolaDX = 0;
+    bolaDY = 0;
+      
+    }
+
+    contador = contador + 1;
+   
 }
+
+
+function reiniciar(){
+    document.location.reload();
+
+}
+function vitoria(){
+    var mensagem = document.getElementById("vitoria");
+    mensagem.style.display = "block";
+    gerarEfeitoSonoro('vitoria.mp3')
+
+    bolaDX = 0;
+    bolaDY = 0;
+
+}
+
+function gerarEfeitoSonoro(som){
+    var contexto = new (window.AudioContext)();
+    var requisicao = new XMLHttpRequest();
+    requisicao.open('GET',som,true);
+    requisicao.responseType = 'arraybuffer';
+
+    requisicao.onload = function(){ 
+        contexto.decodeAudioData(requisicao.response, function( buffer ){ 
+            var fonte = contexto.createBufferSource();
+            fonte.buffer = buffer;
+            fonte.connect(contexto.destination);
+            fonte.start(0);
+         });
+    }
+    requisicao.send();
 }
 
 function desenhar(){
     desenho.clearRect(0 ,0, canvas.width, canvas.height);
     desenharRaquete();
     desenharBola();
-    desenhartijolos();
+    desenharTijolos();
     detectarColisao();
 
     if (bolaX + bolaDX > canvas.width - bolaRadius || bolaX + bolaDX < bolaRadius){
         
         bolaDX = -bolaDX;
+        gerarEfeitoSonoro('bordas.mp3');
 
     }
 
     if(bolaY + bolaDY < bolaRadius){
 
         bolaDY = -bolaDY;
+        gerarEfeitoSonoro('bordas.mp3');
 
-    } else if(bolaY + bolaDY > canvas.height - bolaRadius){
+
+
+    } else if(bolaY + bolaRadius + bolaDY > canvas.height - bolaRadius){
 
         if(bolaX > raqueteX && bolaX < raqueteX + raqueteLargura){
 
@@ -141,7 +283,7 @@ function desenhar(){
 
             }else{
 
-                document.location.reload();
+                gameover();
             }
         }
 
@@ -159,4 +301,11 @@ function desenhar(){
 
     }
 
- desenhar();
+ 
+    
+
+
+
+
+
+desenhar();
